93a427655d2e343fbdffee655281fb0622b4cb5e
Yash <ypt7895@gmail.com>
2023-06-18 13:52:06 +0530

    sends latest xkcd comic

--- bot.py
+++ bot.py
@@ -16,6 +16,12 @@ bot = commands.Bot(command_prefix='heygz ',intents=intents)
 @bot.command(name='xkcd', help='Returns an XKCD comic strip')
 async def getXKCD(ctx):
     response = fetchLatest()
-    await ctx.send(response['num'])
+    num = response['num']
+    d, m, y = response['day'], response['month'], response['year']
+    embd = discord.Embed(title=response['title'],url=f'https://xkcd.com/{num}',description=response['alt'])
+    embd.set_author(name='Randall Munroe')
+    embd.set_image(url=response['img'])
+    embd.set_footer(text=f'{d}/{m}/{y}')
+    await ctx.send(embed=embd)
 
 bot.run(TOKEN)
