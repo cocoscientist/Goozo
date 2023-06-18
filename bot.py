efd62a53570f0bda2f1c020b43f40d413f179c19
Yash <ypt7895@gmail.com>
2023-06-18 13:59:41 +0530

    support for custom edition

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 
 import discord
 from discord.ext import commands
-from fetchxkcd import fetchLatest
+from fetchxkcd import fetchLatest, fetchByNum
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -14,14 +14,18 @@ intents.message_content = True
 bot = commands.Bot(command_prefix='heygz ',intents=intents)
 
 @bot.command(name='xkcd', help='Returns an XKCD comic strip')
-async def getXKCD(ctx):
-    response = fetchLatest()
+async def getXKCD(ctx, edition:int=None):
+    response = None
+    if edition is None:
+        response = fetchLatest()
+    else:
+        response = fetchByNum(edition)
     num = response['num']
     d, m, y = response['day'], response['month'], response['year']
     embd = discord.Embed(title=response['title'],url=f'https://xkcd.com/{num}',description=response['alt'])
     embd.set_author(name='Randall Munroe')
     embd.set_image(url=response['img'])
-    embd.set_footer(text=f'{d}/{m}/{y}')
+    embd.set_footer(text=f'XKCD {num} - {d}/{m}/{y}')
     await ctx.send(embed=embd)
 
 bot.run(TOKEN)
